dc2f2152804fce34aa0b5d49b84850cdd71ed41f
Desinweb <ferbari@gmail.com>
2020-05-05 12:44:25 +0200

    Error nuevaZapatillaHogar.setCodigo(codigo); Error menu nextLine();

--- src/utils/Almacenamiento.java
+++ src/utils/Almacenamiento.java
@@ -1,11 +1,17 @@
 package utils;
 
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+//
 import java.util.Scanner;
 import zapateria.Calzado;
 import zapateria.Zapatos;
 import zapateria.ZapatillasHogar;
 import zapateria.ChangletaSandalias;
 import zapateria.CalzadoDeportivo;
+
 /**
  * Da de Alta ,baja,consulta y listado
  *
@@ -20,6 +26,41 @@ public class Almacenamiento {
     public static void altacalzado() {
         //inicializo el objeto
         //menu con las opciones 
+        Scanner sc = new Scanner(System.in);
+          int opciones;//Variable de opcion de menu
+        do{
+      
+        System.out.println("Menu de alta");
+        System.out.println("0.Salir");
+        System.out.println("1.Alta nuevo zapato");
+        System.out.println("2.Alta nueva zapatilla hogar");
+        System.out.println("Alta nueva changleta o sandalia");
+        System.out.println("Introduce una opción");
+        opciones = sc.nextInt();//
+        switch(opciones){
+            case 1:
+                break;
+                
+            case 2:
+        //nuevaZapatillaHogar
+        ZapatillasHogar nuevaZapatillaHogar = new ZapatillasHogar();
+        System.out.println("Introduce el codigo de la zapatilla de hogar");
+        String codigo=sc.nextLine();
+        nuevaZapatillaHogar.setCodigo(codigo);
+         System.out.println("Introduce el modelo");
+        String modelo=sc.nextLine();
+        nuevaZapatillaHogar.setModelo(modelo);
+        System.out.println("Introduce el Genero");
+        String genero=sc.nextLine();
+        nuevaZapatillaHogar.setGenero(genero);
+       
+        System.out.println("Introduce la talla");
+        int talla=sc.nextInt();
+        nuevaZapatillaHogar.setTalla(talla);
+        break;
+        
+        }
+        }while(opciones!=0);
         Calzado nuevoCalzado = new Calzado() {
         };
         nuevoCalzado.setCodigo("8787hh");
@@ -27,18 +68,13 @@ public class Almacenamiento {
         nuevoCalzado.setGenero("Mujer");
         nuevoCalzado.setModelo("smd54");
         nuevoCalzado.setTalla(35);
-         
+
         //Nuevozapato
         Zapatos nuevoZapato = new Zapatos();
         nuevoZapato.setColor("Rojo");
         nuevoZapato.setMaterial("Piel");
         //case :
-        //nuevaZapatillaHogar
-        ZapatillasHogar nuevaZapatillaHogar = new ZapatillasHogar();
-        nuevaZapatillaHogar.setCodigo("9874");
-        nuevaZapatillaHogar.setTalla(38);
-        nuevaZapatillaHogar.setGenero("mujer");
-        nuevaZapatillaHogar.setModelo("RRRT567");
+        
         //ChangletaSandalias
         ChangletaSandalias nuevaChangletaSandalia = new ChangletaSandalias();
         nuevaChangletaSandalia.getCodigo();
@@ -50,11 +86,17 @@ public class Almacenamiento {
         nuevaChangletaSandalia.getUrlFoto();
         nuevaChangletaSandalia.getFabricante();
         nuevaChangletaSandalia.getTipo();
-                
-        //llamar metodo escribir o RANDOMACCESSFILE
+ /**       
+try{
+archivo=new RandoAccessFile("Calzado.dat","rw");
+}catch(IOException e){
+
+}*/
+  
+
         System.out.println("hola " + nuevoCalzado.getTalla());
         System.out.println("Zapato " + nuevoZapato.getColor() + " El material es: " + nuevoZapato.getMaterial());
-        System.out.println("Zapatilla de hogar " + nuevaZapatillaHogar.getCodigo());
+      
     }
 
     /**
